8d2925163e81eb4b1a2c2c384ee265508c09381b
bee85919 <bee85919@gmail.com>
2023-11-15 16:31:36 +0900

    Update run.sh
    - 옵션 추가
        - num-executors
        - executor-cores
        - executor-memory

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 ## pyspark 로컬 환경에서 실행
-pyspark --conf "spark.driver.bindAddress=127.0.0.1"
+pyspark \
+    --conf "spark.driver.bindAddress=127.0.0.1" \
+    --master local[*] \
+    --num-executors 17 \
+    --executor-cores 5 \
+    --executor-memory 19G \
+
+
+# https://blog.cloudera.com/how-to-tune-your-apache-spark-jobs-part-2/
